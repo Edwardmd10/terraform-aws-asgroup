78d8c489157e912991fb07264b50b483e2ddb56e
Edward DaSilva <99508898+Edwardmd10@users.noreply.github.com>
2022-06-25 18:22:05 -0500

    updated source in module

--- module/main.tf
+++ module/main.tf
@@ -1,5 +1,5 @@
 module asg {
-    source = "../"
+    source = "Edwardmd10/asgroup/aws"
     region           = "us-east-1"
     name_prefix      = "foobar"
     image_id         = "ami-0022f774911c1d690"
